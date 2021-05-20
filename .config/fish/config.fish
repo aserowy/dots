87d79a71c7f56b73b78fc3252fc5081234c686e2
Alexander Serowy <serowy@hotmail.com>
2021-05-20 17:05:17 +0200

    added nvim config path correction
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,11 @@ source ~/.config/fish/alias.fish
 # ssh
 eval (ssh-agent -c)
 
+# neovim
+if test -d c:/tools/cygwin/$HOME/.config/
+    set -x XDG_CONFIG_HOME c:/tools/cygwin/$HOME/.config/
+end
+
 # starship
 if test -d c:/tools/cygwin/$HOME/.config/
     set -x STARSHIP_CONFIG c:/tools/cygwin/$HOME/.config/starship.toml
