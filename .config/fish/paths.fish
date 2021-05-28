2a0f32142339d854ce0ecd129083848daf220567
Alexander Serowy <serowy@hotmail.com>
2021-05-28 09:14:35 +0200

    added paths.fish with cargo support
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

---
+++ .config/fish/paths.fish
@@ -0,0 +1,15 @@
+# cargo
+if test -d $HOME/.cargo/bin
+    fish_add_path $HOME/.cargo/bin
+end
+
+# neovim
+if test -d c:/tools/cygwin/$HOME/.config/
+    set -x XDG_CONFIG_HOME c:/tools/cygwin/$HOME/.config/
+end
+
+# starship
+if test -d c:/tools/cygwin/$HOME/.config/
+    set -x STARSHIP_CONFIG c:/tools/cygwin/$HOME/.config/starship.toml
+end
+
