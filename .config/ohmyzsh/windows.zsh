1b0defe0897564a2b1cd4f5810052472b1904f3a
Alexander Serowy <serowy@hotmail.com>
2021-06-08 06:31:48 +0200

    fixed path for neovim config
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- .config/ohmyzsh/windows.zsh
+++ .config/ohmyzsh/windows.zsh
@@ -1,6 +1,6 @@
 if [ -d "/cygdrive/c/tools/cygwin$HOME/.config/" ]; then
     # neovim
-    alias nvim="nvim -u $HOME/.config/nvim/init.lua"
+    alias nvim="nvim -u C:/tools/cygwin$HOME/.config/nvim/init.lua"
 
     # starship
     export STARSHIP_CONFIG="$HOME/.config/starship.toml"
