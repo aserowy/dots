7337ec25e6f34b5141b6713799b17f3a163ed986
Alexander Serowy <serowy@hotmail.com>
2021-06-23 13:32:37 +0200

    added install.sh
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

---
+++ install.sh
@@ -0,0 +1,17 @@
+curl -L https://nixos.org/nix/install | sh
+
+sudo chmod +x ~/.nix-profile/etc/profile.d/nix.sh
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+nix-shell '<home-manager>' -A install
+nix-env -iA nixpkgs.git
+
+rm -rf ~/nix
+git clone --branch cr-move-to-nix https://github.com/aserowy/dots.git ~/nix
+
+cd ~/.config
+rm -rf nixpkgs
+ln -s ~/nix nixpkgs
