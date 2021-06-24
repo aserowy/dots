36fc0fa19f9502384b5d0a88db529585b760b404
Alexander Serowy <serowy@hotmail.com>
2021-06-24 06:41:22 +0200

    fixed zsh.nix
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -4,19 +4,18 @@
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    dotDir = ".config/zsh"
-      history = {
-    ignoreDups = true;
-    save = 9999;
-    share = true;
-    size = 9999;
-  };
-  oh-my-zsh = {
-    enable = true;
-    plugins = [ "docker", "docker-compose", "git-auto-fetch", "ssh-agent" ];
-  };
-  plugins = [
-    [
+    dotDir = ".config/zsh";
+    history = {
+      ignoreDups = true;
+      save = 9999;
+      share = true;
+      size = 9999;
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "docker", "docker-compose", "git-auto-fetch", "ssh-agent" ];
+    };
+    plugins = [
       {
         name = "zsh-syntax-highlighting";
         src = pkgs.fetchFromGitHub {
@@ -28,97 +27,97 @@
       }
     ];
     sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-  };
-    shellAliases = {
-    update = "sudo apt update && sudo apt -y upgrade";
-
-    # string operations
-    trim = "sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*\$//g'";
-
-    #cargo
-    c = "cargo";
-    ccy = "cargo clippy";
-    cfmt = "cargo fmt";
-    ct = "cargo test";
-    cb = "cargo build";
-    cbr = "cargo build --release";
-
-    # git
-    g = "git";
-
-    gf = "git fetch";
-
-    gcl = "git clone --recurse-submodules -j8";
-
-    gco = "git checkout";
-    gcob = "git checkout -b";
-
-    gb = "git branch";
-    gba = "git branch -a";
-    gbd = "git branch -d";
-
-    gpla = "git pull --autostash --rebase";
-
-    gm = "git merge";
-    gms = "git merge --squash";
-
-    gsur = "git submodule update --remote --rebase";
-
-    ga = "git add";
-    gaa = "git add --all";
-
-    gc = "git commit -v";
-    gcsm = "git commit -s -m";
-
-    gcp = "git cherry-pick";
-
-    gt = "git tag";
-    gta = "git tag -a";
-
-    grsh = "git reset --hard";
-
-    gp = "git push";
-    gpdr = "git push --dry-run";
-    gpod = "git push origin --delete";
-    gpsu = "git push --set-upstream origin";
-    gpt = "git push --tags";
-
-    gst = "git status";
-
-    gd = "git diff";
-
-    # ls
-    ll = "ls -l";
-    la = "ls -a";
-    lla = "ls -la";
-
-    # mstsc
-    # m() {
-    #     param=$(echo "$1" | trim)
-    #     echo "$param"
-    #     if [ -z "$param" ]; then
-    #         mstsc.exe &
-    #     else
-    #         mstsc.exe /v:"$param" &
-    #     fi
-    # }
-
-    # nvim
-    vim = "nvim";
-    vimdiff = "nvim -d";
-
-    # tmux
-    t = "tmux";
-
-    tl = "tmux ls";
-    ts = "tmux new -s";
-    ta = "tmux attach -t";
-    tad = "tmux attach -d -t";
-    tksv = "tmux kill-server";
-    tkss = "tmux kill-session -t";
-    trss = "tmux rename-session";
-  };
+      EDITOR = "nvim";
+      VISUAL = "nvim";
     };
-    }
+    shellAliases = {
+      update = "sudo apt update && sudo apt -y upgrade";
+
+      # string operations
+      trim = "sed -e 's/^[[:space:]]*//g' -e 's/[[:space:]]*\$//g'";
+
+      #cargo
+      c = "cargo";
+      ccy = "cargo clippy";
+      cfmt = "cargo fmt";
+      ct = "cargo test";
+      cb = "cargo build";
+      cbr = "cargo build --release";
+
+      # git
+      g = "git";
+
+      gf = "git fetch";
+
+      gcl = "git clone --recurse-submodules -j8";
+
+      gco = "git checkout";
+      gcob = "git checkout -b";
+
+      gb = "git branch";
+      gba = "git branch -a";
+      gbd = "git branch -d";
+
+      gpla = "git pull --autostash --rebase";
+
+      gm = "git merge";
+      gms = "git merge --squash";
+
+      gsur = "git submodule update --remote --rebase";
+
+      ga = "git add";
+      gaa = "git add --all";
+
+      gc = "git commit -v";
+      gcsm = "git commit -s -m";
+
+      gcp = "git cherry-pick";
+
+      gt = "git tag";
+      gta = "git tag -a";
+
+      grsh = "git reset --hard";
+
+      gp = "git push";
+      gpdr = "git push --dry-run";
+      gpod = "git push origin --delete";
+      gpsu = "git push --set-upstream origin";
+      gpt = "git push --tags";
+
+      gst = "git status";
+
+      gd = "git diff";
+
+      # ls
+      ll = "ls -l";
+      la = "ls -a";
+      lla = "ls -la";
+
+      # mstsc
+      # m() {
+      #     param=$(echo "$1" | trim)
+      #     echo "$param"
+      #     if [ -z "$param" ]; then
+      #         mstsc.exe &
+      #     else
+      #         mstsc.exe /v:"$param" &
+      #     fi
+      # }
+
+      # nvim
+      vim = "nvim";
+      vimdiff = "nvim -d";
+
+      # tmux
+      t = "tmux";
+
+      tl = "tmux ls";
+      ts = "tmux new -s";
+      ta = "tmux attach -t";
+      tad = "tmux attach -d -t";
+      tksv = "tmux kill-server";
+      tkss = "tmux kill-session -t";
+      trss = "tmux rename-session";
+    };
+  };
+}
