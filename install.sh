2e90c42822f516c27361641312472f6fc7f3d54a
Alexander Serowy <serowy@hotmail.com>
2021-06-24 07:25:12 +0200

    only add shell to shells once
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

--- install.sh
+++ install.sh
@@ -19,5 +19,7 @@ ln -s ~/nix nixpkgs
 cd ~/nix
 home-manager switch
 
-echo "~/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
+if grep -q "~/.nix-profile/bin/zsh" "/etc/shells"; then
+    echo "~/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
+fi
 sudo chsh -s ~/.nix-profile/bin/zsh $(whoami)
