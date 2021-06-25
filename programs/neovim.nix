3d5a32ee9be7d25085bd8d3e5a03126854059e50
Alexander Serowy <serowy@hotmail.com>
2021-06-25 06:15:28 +0200

    added nvim without configuration
    Signed-off-by: Alexander Serowy <serowy@hotmail.com>

---
+++ programs/neovim.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-nightly;
+  };
+}
