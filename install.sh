42da4f0366508d6d3e25ffc8d3b518b234f90c6e
aserowy <serowy@hotmail.com>
2021-06-29 12:40:31 +0200

    added exits to install
    Signed-off-by: aserowy <serowy@hotmail.com>

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ nix-env -iA nixpkgs.git
 
 rm -rf ~/nix
 git clone --recurse-submodules -j8 https://github.com/aserowy/dots.git ~/nix
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
 
 nix-env --uninstall nixpkgs.git
 
@@ -20,6 +23,9 @@ ln -s ~/nix nixpkgs
 
 cd ~/nix
 home-manager switch
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
 
 git remote set-url origin git@github.com:aserowy/dots.git
 
@@ -27,3 +33,5 @@ if ! grep -q "~/.nix-profile/bin/zsh" "/etc/shells"; then
     echo "~/.nix-profile/bin/zsh" | sudo tee -a /etc/shells
 fi
 sudo chsh -s ~/.nix-profile/bin/zsh $(whoami)
+
+exit 0
