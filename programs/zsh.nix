b8c31ad1c35467a7c5386ce2f8b12f12b985c6cd
aserowy <serowy@hotmail.com>
2021-07-27 15:45:14 +0200

    added cargo to zsh
    Signed-off-by: aserowy <serowy@hotmail.com>

--- programs/zsh.nix
+++ programs/zsh.nix
@@ -36,7 +36,7 @@
     '';
     oh-my-zsh = {
       enable = true;
-      plugins = [ "docker" "docker-compose" "git-auto-fetch" "ssh-agent" ];
+      plugins = [ "cargo" "docker" "docker-compose" "git-auto-fetch" "ssh-agent" ];
     };
     plugins = [
       {
