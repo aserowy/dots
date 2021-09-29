773ce8a14940eb19d0a62f7c6704b15d65da8f1a
aserowy <serowy@hotmail.com>
2021-09-29 07:36:32 +0200

    testing with args
    Signed-off-by: aserowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -11,9 +11,9 @@
 
   outputs = { self, nixpkgs, home-manager, ... }: {
     nixosModules = {
-      "serowy@desktop-nixos" = home-manager.nixosModule {
+      "serowy@desktop-nixos" = { config, ... }: home-manager.nixosModule {
+        config = config;
         home-manager.useUserPackages = true;
-
         home-manager.users.serowy = import ./environments/wsl.nix;
       };
     };
