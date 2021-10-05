468653a046efc2f8d7ca1fcdfcf63bbeecf59f9d
aserowy <serowy@hotmail.com>
2021-10-05 06:07:47 +0200

    added edge overlay
    Signed-off-by: aserowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -7,57 +7,65 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, home-manager, nixpkgs, nur, ... }: {
-    /* nixosModules = {
-      "serowy@desktop-nixos" = ({ config, utils, ... }: home-manager.nixosModule {
+  outputs = { self, home-manager, nixpkgs, nur, ... }:
+    let
+      nixpkgs.overlays = [
+        nur.overlay
+        import ./programs/edge-overlay
+      ];
+    in
+    {
+      /* nixosModules = {
+        "serowy@desktop-nixos" = ({ config, utils, ... }: home-manager.nixosModule {
         pkgs = nixpkgs;
         lib = nixpkgs.lib;
         inherit config utils;
 
         home-manager = {
-          useUserPackages = true;
-          users.serowy = import ./environments/desktop.nix;
+        useUserPackages = true;
+        users.serowy = import ./environments/desktop.nix;
         };
-      });
-    }; */
-    homeConfigurations = {
-      "serowy@DESKTOP-2F0CTGF" = home-manager.lib.homeManagerConfiguration {
-        configuration = { config, pkgs, ... }: {
-          imports = [
-            ./environments/wsl.nix
-          ];
+        });
+        }; */
+      homeConfigurations = {
+        "serowy@DESKTOP-2F0CTGF" = home-manager.lib.homeManagerConfiguration {
+          configuration = { config, pkgs, ... }: {
+            imports = [
+              ./environments/wsl.nix
+            ];
+          };
+          homeDirectory = "/home/serowy";
+          stateVersion = "21.05";
+          system = "x86_64-linux";
+          username = "serowy";
         };
-        homeDirectory = "/home/serowy";
-        stateVersion = "21.05";
-        system = "x86_64-linux";
-        username = "serowy";
-      };
-      "serowy@desktop-nixos" = home-manager.lib.homeManagerConfiguration {
-        configuration = { config, pkgs, ... }: {
-          imports = [
-            ./environments/include_homemanager.nix
+        "serowy@desktop-nixos" = home-manager.lib.homeManagerConfiguration {
+          configuration = { config, pkgs, ... }: {
+            imports = [
+              ./environments/include_homemanager.nix
 
-            ./environments/desktop.nix
-          ];
+              ./environments/desktop.nix
+            ];
+          };
+          homeDirectory = "/home/serowy";
+          stateVersion = "21.05";
+          system = "x86_64-linux";
+          username = "serowy";
         };
-        homeDirectory = "/home/serowy";
-        stateVersion = "21.05";
-        system = "x86_64-linux";
-        username = "serowy";
-      };
-      "serowy@DESKTOP-UVAKAQL" = home-manager.lib.homeManagerConfiguration {
-        configuration = { config, pkgs, ... }: {
-          imports = [
-            ./environments/wsl-work.nix
-          ];
+        "serowy@DESKTOP-UVAKAQL" = home-manager.lib.homeManagerConfiguration {
+          configuration = { config, pkgs, ... }: {
+            imports = [
+              ./environments/wsl-work.nix
+            ];
+          };
+          homeDirectory = "/home/serowy";
+          stateVersion = "21.05";
+          system = "x86_64-linux";
+          username = "serowy";
         };
-        homeDirectory = "/home/serowy";
-        stateVersion = "21.05";
-        system = "x86_64-linux";
-        username = "serowy";
       };
     };
-  };
 }
--- modules/browsing.nix
+++ modules/browsing.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
   imports = [
-    pkgs.firefox
+    ../programs/edge.nix
   ];
 }
