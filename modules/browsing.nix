0e32cc25c423193318784108d50e1f8c83458d7e
aserowy <serowy@hotmail.com>
2021-10-16 08:58:53 +0200

    added pwa for outlook and teams
    Signed-off-by: aserowy <serowy@hotmail.com>

--- modules/browsing.nix
+++ modules/browsing.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    (pkgs.writeShellScriptBin "outlook" ''
+      ${edge}/bin/microsoft-edge-beta \
+        --enable-features=UseOzonePlatform \
+        --ozone-platform=wayland \
+        --enable-features=WebRTCPipeWireCapturer \
+        --app=https://www.outlook.com \
+        --new-window
+    '')
+  ];
+
   imports = [
     ../programs/edge.nix
   ];
--- programs/teams.nix
+++ programs/teams.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    pkgs.teams
+    # BUG: inprivate and app mode will not open anything
+    # --app=https://teams.microsoft.com 
+    (pkgs.writeShellScriptBin "teams" ''
+      ${edge}/bin/microsoft-edge-beta \
+        --enable-features=UseOzonePlatform \
+        --ozone-platform=wayland \
+        --enable-features=WebRTCPipeWireCapturer \
+        --new-window \
+        --inprivate \
+        https://teams.microsoft.com
+    '')
   ];
 }
