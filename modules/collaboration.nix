7b3dd026cf1da2a7efd1f389828c3f4d40479b6b
aserowy <serowy@hotmail.com>
2021-10-17 12:59:36 +0200

    added further webapps for colaboration
    Signed-off-by: aserowy <serowy@hotmail.com>

--- modules/collaboration.nix
+++ modules/collaboration.nix
@@ -1,6 +1,34 @@
 { config, pkgs, ... }:
 {
-  imports = [
-    ../programs/teams.nix
+  home.packages = with pkgs; [
+    (pkgs.writeShellScriptBin "discord" ''
+      ${edge}/bin/microsoft-edge-beta \
+        --enable-features=UseOzonePlatform \
+        --ozone-platform=wayland \
+        --enable-features=WebRTCPipeWireCapturer \
+        --app=https://www.discord.app \
+        --new-window
+    '')
+
+    (pkgs.writeShellScriptBin "teams" ''
+      ${edge}/bin/microsoft-edge-beta \
+        --enable-features=UseOzonePlatform \
+        --ozone-platform=wayland \
+        --enable-features=WebRTCPipeWireCapturer \
+        --new-window \
+        --inprivate \
+        https://teams.microsoft.com
+        # BUG: inprivate and app mode will not open anything
+        # --app=https://teams.microsoft.com 
+    '')
+
+    (pkgs.writeShellScriptBin "whatsapp" ''
+      ${edge}/bin/microsoft-edge-beta \
+        --enable-features=UseOzonePlatform \
+        --ozone-platform=wayland \
+        --enable-features=WebRTCPipeWireCapturer \
+        --app=https://web.whatsapp.com \
+        --new-window
+    '')
   ];
 }
