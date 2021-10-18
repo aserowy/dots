1692963a6beb36999b4a351f5c442e64d110f046
aserowy <serowy@hotmail.com>
2021-10-18 08:48:40 +0200

    fixed dunst config
    Signed-off-by: aserowy <serowy@hotmail.com>

--- programs/dunst.nix
+++ programs/dunst.nix
@@ -1,11 +1,52 @@
 { config, pkgs, ... }:
 {
-  home.file.".config/dunst/dunstrc" = {
-    source = ./dunst.config;
-  };
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        alignment = "center";
+        always_run_script = "true";
+        corner_radius = 2;
+        follow = "none";
+        font = "FiraCode Nerd Font Mono 10";
+        format = "<b>%s</b>\\n%b";
+        frame_color = "#61afef";
+        frame_width = 1;
+        geometry = "400x5-20-20";
+        horizontal_padding = 8;
+        icon_position = "left";
+        markup = "full";
+        max_icon_size = 32;
+        mouse_left_click = "do_action,close_current";
+        mouse_right_click = "close_current";
+        separator_color = "frame";
+        separator_height = 1;
+        stack_duplicates = "true";
+        startup_notification = "false";
+        transparency = 0;
+        word_wrap = "yes";
 
-  home.packages = with pkgs; [
-    dunst
-  ];
+        dmenu = "${pkgs.rofi}/bin/rofi -dmenu";
+        browser = "${pkgs.edge}/bin/microsoft-edge-beta";
+      };
+
+      urgency_low = {
+        background = "#282c34";
+        foreground = "#ABB2BF";
+        timeout = 10;
+      };
+      urgency_normal = {
+        background = "#282c34";
+        foreground = "#ABB2BF";
+        timeout = 10;
+      };
+      urgency_critical = {
+        background = "#282c34";
+        foreground = "#e06c75";
+        frame_color = "#e06c75";
+        timeout = 0;
+      };
+    };
+  };
 }
 
