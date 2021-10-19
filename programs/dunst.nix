3cbdb699caa156c1308528637bed7ee94458fe64
aserowy <serowy@hotmail.com>
2021-10-19 06:12:41 +0200

    restyled waybar and dunst
    Signed-off-by: aserowy <serowy@hotmail.com>

--- programs/dunst.nix
+++ programs/dunst.nix
@@ -6,21 +6,20 @@
       global = {
         alignment = "center";
         always_run_script = "true";
-        corner_radius = 2;
+        corner_radius = 0;
         follow = "none";
         font = "FiraCode Nerd Font Mono 10";
         format = "<b>%s</b>\\n%b";
-        frame_color = "#61afef";
-        frame_width = 1;
+        frame_color = "#23272e";
+        frame_width = 10;
         geometry = "400x5-20-20";
-        horizontal_padding = 8;
         icon_position = "left";
         markup = "full";
         max_icon_size = 32;
         mouse_left_click = "do_action,close_current";
         mouse_right_click = "close_current";
         separator_color = "frame";
-        separator_height = 1;
+        separator_height = 10;
         stack_duplicates = "true";
         startup_notification = "false";
         transparency = 0;
@@ -31,19 +30,18 @@
       };
 
       urgency_low = {
-        background = "#282c34";
+        background = "#23272e";
         foreground = "#ABB2BF";
         timeout = 10;
       };
       urgency_normal = {
-        background = "#282c34";
+        background = "#23272e";
         foreground = "#ABB2BF";
         timeout = 10;
       };
       urgency_critical = {
-        background = "#282c34";
+        background = "#23272e";
         foreground = "#e06c75";
-        frame_color = "#e06c75";
         timeout = 0;
       };
     };
