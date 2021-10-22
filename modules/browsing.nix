ee5c2e3ef3ba75560b65461a8fc2be6211a8e75c
aserowy <serowy@hotmail.com>
2021-10-22 14:04:00 +0200

    unset pwas
    Signed-off-by: aserowy <serowy@hotmail.com>

--- modules/browsing.nix
+++ modules/browsing.nix
@@ -1,15 +1,5 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    (pkgs.writeShellScriptBin "outlook" ''
-      ${edge}/bin/microsoft-edge-beta \
-        --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer \
-        --ozone-platform=wayland \
-        --app=https://www.outlook.com \
-        --new-window
-    '')
-  ];
-
   imports = [
     ../programs/edge.nix
   ];
--- modules/collaboration.nix
+++ modules/collaboration.nix
@@ -3,29 +3,12 @@
   home.packages = with pkgs; [
     google-chrome
 
-    (pkgs.writeShellScriptBin "discord" ''
-      ${edge}/bin/microsoft-edge-beta \
-        --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer \
-        --ozone-platform=wayland \
-        --app=https://www.discord.app \
-        --new-window
-    '')
-
     (pkgs.writeShellScriptBin "teams" ''
       ${google-chrome}/bin/google-chrome-stable \
         --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer \
         --ozone-platform=wayland \
         --new-window \
         --app=https://teams.microsoft.com 
-    '')
-
-    (pkgs.writeShellScriptBin "whatsapp" ''
-      ${edge}/bin/microsoft-edge-beta \
-        --enable-features=UseOzonePlatform \
-        --ozone-platform=wayland \
-        --enable-features=WebRTCPipeWireCapturer \
-        --app=https://web.whatsapp.com \
-        --new-window
     '')
   ];
 }
