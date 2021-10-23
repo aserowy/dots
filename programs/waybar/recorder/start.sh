170c0bb8a944af4d2728657a62719059f56909cd
aserowy <serowy@hotmail.com>
2021-10-23 14:11:58 +0200

    added rec
    Signed-off-by: aserowy <serowy@hotmail.com>

---
+++ programs/waybar/recorder/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wf-recorder -o $(swaymsg -r -t get_outputs  | jq -r '.[] | select(.focused == true).name') -f $HOME/videos/$(date -Is).mp4 &> /dev/null &
+notify-send ' screen cap started'
+sleep .1
+
+# send signal to update monitor 
+pkill -RTMIN+8 waybar
---
+++ programs/waybar/recorder/stop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+pkill -SIGINT wf-recorder && notify-send ' screen cap ended'
+sleep .1
+
+# send signal to update monitor
+pkill -RTMIN+8 waybar
