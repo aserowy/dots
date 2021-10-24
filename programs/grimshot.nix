51fa94ece28cc067675c1f6f9739a8b1e723b1d8
aserowy <serowy@hotmail.com>
2021-10-24 14:39:21 +0200

    added swappy config
    Signed-off-by: aserowy <serowy@hotmail.com>

--- programs/grimshot.nix
+++ programs/grimshot.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 {
+  home.file.".config/swappy/config" = {
+    text = ''
+      [Default]
+      save_dir=$HOME/pictures
+      save_filename_format=screenshot-%Y%m%d-%H%M%S.png
+      show_panel=false
+      line_size=5
+      text_size=20
+      text_font=sans-serif
+    '';
+  };
+
   home.packages = with pkgs; [
     swappy
     sway-contrib.grimshot
