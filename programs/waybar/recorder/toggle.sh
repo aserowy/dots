2cb03feb6990fb415199b5fd2290fc91d10f5f51
aserowy <serowy@hotmail.com>
2021-10-25 18:00:19 +0200

    toggle recorder
    Signed-off-by: aserowy <serowy@hotmail.com>

---
+++ programs/waybar/recorder/toggle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# TODO: script to toggle recording
+
+if pgrep wf-recorder &> /dev/null
+then
+    pkill -SIGINT wf-recorder && notify-send ' screen cap ended'
+    sleep .1
+else
+    wf-recorder -o $(swaymsg -r -t get_outputs  | jq -r '.[] | select(.focused == true).name') -f $HOME/videos/$(date -Is).mp4 &> /dev/null &
+    notify-send ' screen cap started'
+    sleep .1
+fi 
+
+# send signal to update monitor 
+pkill -RTMIN+8 waybar
