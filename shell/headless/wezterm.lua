d9690a848773a0eae704211f153f178e029b7238
aserowy <serowy@hotmail.com>
2021-11-16 13:22:13 +0100

    powershell on wez as default on windows
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell/headless/wezterm.lua
+++ shell/headless/wezterm.lua
@@ -149,7 +149,7 @@ local M = {
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	M.default_prog = { "powershell.exe" }
+	M.default_prog = { "pwsh.exe" }
 	M.leader = { mods = "CTRL|ALT", key = "t" }
 else
 	M.leader = { mods = "CTRL", key = "t" }
