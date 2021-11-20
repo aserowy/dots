af2ac472c47bf32a32b394a70c8e17fe47954279
aserowy <serowy@hotmail.com>
2021-11-20 09:00:06 +0100

    unified ssh config
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell/headless/ssh.nix
+++ shell/headless/ssh.nix
@@ -7,17 +7,23 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      "desktop-workstation" = {
+        hostname = "desktop-workstation";
+        user = "serowy";
+        identitiesOnly = true;
+        identityFile = " ~/.ssh/internal_serowy_ed25519";
+      };
       "github.com" = {
         hostname = "github.com";
         user = "git";
         identitiesOnly = true;
         identityFile = " ~/.ssh/github_aserowy_ed25519";
       };
-      "192.168.178.53" = {
-        hostname = "192.168.178.53";
+      "homeassistant" = {
+        hostname = "homeassistant";
         user = "serowy";
         identitiesOnly = true;
-        identityFile = " ~/.ssh/desktop-nix_serowy_ed25519";
+        identityFile = " ~/.ssh/internal_serowy_ed25519";
       };
     };
   };
