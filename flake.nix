0b09c928348865cb6d986788a4bdd06e44b5f925
aserowy <serowy@hotmail.com>
2022-01-06 12:18:41 +0100

    fixed hm flake config
    Signed-off-by: aserowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -25,93 +25,99 @@
         ];
       };
     in
-    {
-      devShell.x86_64-linux = import ./.dev { pkgs = packages.legacyPackages.x86_64-linux; };
+      {
+        devShell.x86_64-linux = import ./.dev { pkgs = packages.legacyPackages.x86_64-linux; };
 
-      homeConfigurations = {
-        "serowy@DESKTOP-UVAKAQL" = home.lib.homeManagerConfiguration {
-          configuration = { config, pkgs, ... }: {
-            imports = [
-              ./home/environments/wsl-work.nix
+        homeConfigurations = {
+          "serowy@DESKTOP-UVAKAQL" = home.lib.homeManagerConfiguration {
+            configuration = { config, pkgs, ... }: {
+              imports = [
+                ./home/environments/wsl-work.nix
+              ];
+            };
+            homeDirectory = "/home/serowy";
+            pkgs = import nixpkgs {
+              overlays = [
+                (import ./pkgs)
+              ];
+              system = "x86_64-linux";
+            };
+            stateVersion = "21.05";
+            system = "x86_64-linux";
+            username = "serowy";
+          };
+        };
+
+        nixosConfigurations = {
+          desktop-workstation = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              {
+                nixpkgs.overlays = [
+                  fenix.overlay
+                  (import ./pkgs)
+                ];
+              }
+
+              ./system/workstation
+              ./shell/i3
+              ./users/serowy.nix
+
+              home.nixosModule
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.serowy = import ./home/environments/i3.nix;
+                };
+              }
             ];
           };
-          homeDirectory = "/home/serowy";
-          stateVersion = "21.05";
-          system = "x86_64-linux";
-          username = "serowy";
+
+          desktop-nuc = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              {
+                nixpkgs.overlays = [
+                  fenix.overlay
+                  (import ./pkgs)
+                ];
+              }
+
+              ./system/intel_nuc
+              ./shell/i3
+              ./users/serowy.nix
+
+              home.nixosModule
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.serowy = import ./home/environments/i3.nix;
+                };
+              }
+            ];
+          };
+
+          homeassistant = nixpkgs.lib.nixosSystem {
+            system = "aarch64-linux";
+            modules = [
+              hardware.nixosModules.raspberry-pi-4
+
+              ./system/homeassistant
+              ./shell/headless
+              ./users/serowy.nix
+
+              home.nixosModule
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.serowy = import ./home/environments/headless.nix;
+                };
+              }
+            ];
+          };
         };
       };
-
-      nixosConfigurations = {
-        desktop-workstation = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            {
-              nixpkgs.overlays = [
-                fenix.overlay
-                (import ./pkgs)
-              ];
-            }
-
-            ./system/workstation
-            ./shell/i3
-            ./users/serowy.nix
-
-            home.nixosModule
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.serowy = import ./home/environments/i3.nix;
-              };
-            }
-          ];
-        };
-
-        desktop-nuc = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            {
-              nixpkgs.overlays = [
-                fenix.overlay
-                (import ./pkgs)
-              ];
-            }
-
-            ./system/intel_nuc
-            ./shell/i3
-            ./users/serowy.nix
-
-            home.nixosModule
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.serowy = import ./home/environments/i3.nix;
-              };
-            }
-          ];
-        };
-
-        homeassistant = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          modules = [
-            hardware.nixosModules.raspberry-pi-4
-
-            ./system/homeassistant
-            ./shell/headless
-            ./users/serowy.nix
-
-            home.nixosModule
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.serowy = import ./home/environments/headless.nix;
-              };
-            }
-          ];
-        };
-      };
-    };
 }
