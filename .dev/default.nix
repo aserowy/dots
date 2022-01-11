69b128fb21d949f889991a66b51dc1ae0c17072f
aserowy <serowy@hotmail.com>
2022-01-11 07:04:10 +0100

    moved to nix develop
    Signed-off-by: aserowy <serowy@hotmail.com>

--- .dev/default.nix
+++ .dev/default.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }: with pkgs; mkShell {
   buildInputs = [
+    nodePackages.markdownlint-cli
+    nodePackages.prettier
     nvfetcher
+    stylua
+    sumneko-lua-language-server
+    rnix-lsp
   ];
+  shellHook = ''
+    # format
+    alias fmt="prettier --write README.md && nixpkgs-fmt ."
+
+    # update wezterm config
+    alias uw="cp ./shell/headless/wezterm.lua /mnt/c/Users/serowy/.wezterm.lua && cp ./shell/headless/alacritty.yml /mnt/c/Users/serowy/AppData/Roaming/alacritty/alacritty.yml"
+  '';
 }
