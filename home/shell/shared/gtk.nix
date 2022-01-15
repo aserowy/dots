0f398a78b44b595f1fa3fe752b494f06c2b5b999
aserowy <serowy@hotmail.com>
2022-01-15 09:37:22 +0100

    reworked gtk theme
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/shell/shared/gtk.nix
+++ home/shell/shared/gtk.nix
@@ -1,45 +1,33 @@
 { config, pkgs, ... }:
 let
   theme = "Materia-dark";
-  font = "FiraCode Nerd Font Mono 10";
-  cursor = "Paper";
+  font = "FiraCode Nerd Font Mono";
   icon = "Paper";
 in
 {
   home.packages = with pkgs; [
     libappindicator-gtk3
-    materia-theme
-    paper-icon-theme
     xdg_utils
   ];
 
   gtk = {
     enable = true;
-#    iconTheme = {
-#      name = "Adwaita-dark";
-#      package = pkgs.gnome3.adwaita-icon-theme;
-#    };
-#    theme = {
-#      name = "Adwaita-dark";
-#      package = pkgs.gnome3.adwaita-icon-theme;
-#    };
+    font = {
+      name = font;
+      size = 10;
+    };
+    iconTheme = {
+      name = icon;
+      package = pkgs.paper-icon-theme;
+    };
+    theme = {
+      name = theme;
+      package = pkgs.materia-theme;
+    };
   };
 
   xdg = {
     enable = true;
-
-    configFile."gtk-3.0/settings.ini".text = ''
-      [Settings]
-      gtk-font-name=${font}
-      gtk-icon-theme-name=${icon}
-      gtk-theme-name=${theme}
-      gtk-cursor-theme-name=${cursor}
-      gtk-xft-antialias=1
-      gtk-xft-hinting=1
-      gtk-xft-hintstyle=hintfull
-      gtk-xft-rgba=none
-    '';
-
     userDirs = {
       enable = true;
       desktop = "$HOME/desktop";
