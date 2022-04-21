d0448cb7d58431fa8cb24d5a8f05a90963e7c5f2
aserowy <serowy@hotmail.com>
2022-04-21 18:01:12 +0200

    fixed spotify status script for polybar
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell/i3/polybar/src/get_spotify_status.sh
+++ shell/i3/polybar/src/get_spotify_status.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# The name of polybar bar which houses the main spotify module and the control modules.
-PARENT_BAR="main"
-PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
-
 # Set the source audio player here.
 # Players supporting the MPRIS spec are supported.
 # Examples: spotify, vlc, chrome, mpv and others.
@@ -15,14 +11,6 @@ PLAYER="spotify"
 # Eg. {{ artist }} - {{ album }} - {{ title }}
 # See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
 FORMAT="{{ title }} - {{ artist }}"
-
-# Sends $2 as message to all polybar PIDs that are part of $1
-update_hooks() {
-    while IFS= read -r id
-    do
-        polybar-msg -p "$id" hook spotify-play-pause $2 1>/dev/null 2>&1
-    done < <(echo "$1")
-}
 
 PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
 EXIT_CODE=$?
@@ -43,13 +31,11 @@ else
         echo "No music is playing"
     elif [ "$STATUS" = "Paused"  ]
     then
-        update_hooks "$PARENT_BAR_PID" 2
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     elif [ "$STATUS" = "No player is running"  ]
     then
         echo ""
     else
-        update_hooks "$PARENT_BAR_PID" 1
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     fi
 fi
