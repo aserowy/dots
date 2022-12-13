323d6a0911c19ffb490764d73763ced356b221b3
aserowy <serowy@hotmail.com>
2022-12-13 17:10:20 +0100

    removed concurrent database reload from clamav
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell/shared/clamav.nix
+++ shell/shared/clamav.nix
@@ -2,6 +2,12 @@
 {
   services.clamav = {
     updater.enable = true;
-    daemon.enable = true;
+
+    daemon = {
+      enable = true;
+      settings = {
+        ConcurrentDatabaseReload = false;
+      };
+    };
   };
 }
