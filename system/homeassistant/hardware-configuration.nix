7b1a8e09d75620894a5c8c134543d93acc7f5cb8
aserowy <serowy@hotmail.com>
2022-12-27 18:03:21 +0100

    new partitions for hassio
    Signed-off-by: aserowy <serowy@hotmail.com>

--- system/homeassistant/hardware-configuration.nix
+++ system/homeassistant/hardware-configuration.nix
@@ -7,8 +7,19 @@
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-label/NIXOS_SD";
+      device = "/dev/disk/by-label/root";
       fsType = "ext4";
       options = [ "noatime" ];
     };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [
+      { device = "/dev/disk/by-label/swap"; }
+    ];
 }
