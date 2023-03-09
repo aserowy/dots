20c8f34da71fc6a57b5c2f738e0dbc2527f352cc
aserowy <aserowy@users.noreply.github.com>
2023-03-09 19:18:39 +0000

    updated shake

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -95,11 +95,11 @@
   };
   vscode-extensions-material-theme = {
     pname = "vscode-extensions-material-theme";
-    version = "3.15.6";
+    version = "3.15.8";
     src = fetchurl {
-      url = "https://zhuangtongfa.gallery.vsassets.io/_apis/public/gallery/publisher/zhuangtongfa/extension/material-theme/3.15.6/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
-      name = "material-theme-3.15.6.zip";
-      sha256 = "sha256-LViiHGDJjtQwz5O6ZZrAApi2F1rISZvcggCl8Y3nzTA=";
+      url = "https://zhuangtongfa.gallery.vsassets.io/_apis/public/gallery/publisher/zhuangtongfa/extension/material-theme/3.15.8/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
+      name = "material-theme-3.15.8.zip";
+      sha256 = "sha256-PwWGs9KRfV3qpYbgdiw8FYvnkaJQ2VW2H6p6+umk7eg=";
     };
     name = "material-theme";
     publisher = "zhuangtongfa";
