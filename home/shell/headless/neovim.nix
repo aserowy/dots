b53f25b98fe60c87772721e6d78c6725a8e4afe9
aserowy <serowy@hotmail.com>
2023-05-11 07:45:42 +0200

    neocode: set default theme to hardhacker
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/shell/headless/neovim.nix
+++ home/shell/headless/neovim.nix
@@ -7,7 +7,7 @@
   home.file.".config/nvim/" = {
     recursive = true;
     source = pkgs.neocode.override {
-      theme = "onedark";
+      theme = "hardhacker";
       style = "dark";
     };
   };
