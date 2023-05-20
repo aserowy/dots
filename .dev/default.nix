134abb611284bb2a7f96af4d3febbc94197e327d
aserowy <serowy@hotmail.com>
2023-05-20 16:06:42 +0200

    dev: added formatter
    Signed-off-by: aserowy <serowy@hotmail.com>

--- .dev/default.nix
+++ .dev/default.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }: with pkgs; mkShell {
   buildInputs = [
+    nil
+    nixpkgs-fmt
     nodePackages.markdownlint-cli
     nodePackages.prettier
     nvfetcher
     stylua
     sumneko-lua-language-server
-    nil
   ];
 }
