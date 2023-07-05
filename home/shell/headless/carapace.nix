41b5e28d408dc8ab23f0fdb911e94b8cf0ed3772
Serowy, Alexander (EXT/ADMIN) <Alexander.Serowy.ADMIN@union-investment.de>
2023-07-05 10:56:32 +0200

    nushell: added alias for git merge
    Signed-off-by: Serowy, Alexander (EXT/ADMIN) <Alexander.Serowy.ADMIN@union-investment.de>

--- home/shell/headless/carapace.nix
+++ home/shell/headless/carapace.nix
@@ -1,21 +1,23 @@
 { pkgs, ... }: with builtins;
 let
   aliases = ''
-    alias gfe = git fetch
-    alias gfea = git fetch --all
+    alias gad = git add
+    alias gada = git add --all
+    alias gbr = git branch
     alias gcl = git clone --recurse-submodules -j8
     alias gco = git checkout
-    alias gbr = git branch
-    alias gpl = git pull --autostash --rebase
-    alias gada = git add --all
     alias gcm = git commit -s -m
-    alias grs = git reset
-    alias gta = git tag
-    alias gtam = git tag -a -m
+    alias gdf = git diff
+    alias gfe = git fetch
+    alias gfea = git fetch --all
+    alias gmg = git merge
+    alias gpl = git pull --autostash --rebase
     alias gpu = git push
     alias gput = git push --tags
+    alias grs = git reset
     alias gst = git status
-    alias gdf = git diff
+    alias gta = git tag
+    alias gtam = git tag -a -m
   '';
 
   string_filter = x: isString x && x != "";
