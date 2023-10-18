4e9fd711af393262f3985e71f46cf153c5f923d9
aserowy <serowy@hotmail.com>
2023-10-18 13:33:56 +0200

    wezterm: no tab bar for one tab
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/modules/wezterm/wezterm.lua
+++ home/modules/wezterm/wezterm.lua
@@ -97,6 +97,7 @@ local M = {
         },
     },
     use_fancy_tab_bar = false,
+    hide_tab_bar_if_only_one_tab = true,
     inactive_pane_hsb = {
         saturation = 0.7,
         brightness = 0.6,
