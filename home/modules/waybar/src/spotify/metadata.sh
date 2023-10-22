5190c9bb2736bc59a95150f7802b9ff2e9139e8f
aserowy <serowy@hotmail.com>
2023-10-22 15:20:24 +0200

    waybar: reduced playerctl calls on not running spotify
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/modules/waybar/src/spotify/metadata.sh
+++ home/modules/waybar/src/spotify/metadata.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 status=$(playerctl -p spotify status)
-artist=$(playerctl -p spotify metadata xesam:artist)
-title=$(playerctl -p spotify metadata xesam:title)
-album=$(playerctl -p spotify metadata xesam:album)
 
 if [[ -z $status ]] 
 then
    # spotify is dead, we should die to.
    exit
 fi
+
+artist=$(playerctl -p spotify metadata xesam:artist)
+title=$(playerctl -p spotify metadata xesam:title)
+album=$(playerctl -p spotify metadata xesam:album)
 
 if [[ $status == "Playing" ]]
 then
