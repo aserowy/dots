f1a1c304fa8537514b1a112e58e28aedb188a109
aserowy <serowy@hotmail.com>
2023-10-28 13:09:20 +0200

    edge: added options for default browser and xdgassociations
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/components/edge/default.nix
+++ home/components/edge/default.nix
@@ -15,34 +15,42 @@ in
         If enabled, edge gets set as default browser in dunst.
       '';
     };
+
+    setDefaultBrowserSessionVariable = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        If enabled, edge gets set as default browser with DEFAULT_BROWSER.
+      '';
+    };
+
+    enableXdgAssociations = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        If enabled, edge gets set as default browser in xdg.
+      '';
+    };
   };
 
   config = mkIf cnfg.enable {
-    home = {
-      sessionVariables = {
-        /* set edge as default browser for electron apps */
-        DEFAULT_BROWSER = "${pkgs.microsoft-edge-beta}/bin/microsoft-edge-beta";
-      };
-
-      packages = with pkgs; [
-        microsoft-edge-beta
-      ];
-    };
-
-    xdg = {
-      mimeApps = {
-        enable = true;
-        associations.added = {
-          "text/html" = "microsoft-edge-beta.desktop";
-          "x-scheme-handler/http" = "microsoft-edge-beta.desktop";
-          "x-scheme-handler/https" = "microsoft-edge-beta.desktop";
-          "x-scheme-handler/about" = "microsoft-edge-beta.desktop";
-          "x-scheme-handler/unknown" = "microsoft-edge-beta.desktop";
-        };
-      };
-    };
+    home.packages = with pkgs; [
+      microsoft-edge-beta
+    ];
 
     home.components.dunst.browserPath = mkIf cnfg.enableDunstIntegration
       "${pkgs.microsoft-edge-beta}/bin/microsoft-edge-beta";
+
+    /* set edge as default browser for electron apps */
+    home.sessionVariables.DEFAULT_BROWSER = mkIf cnfg.setDefaultBrowserSessionVariable
+      "${pkgs.microsoft-edge-beta}/bin/microsoft-edge-beta";
+
+    xdg.mimeApps.associations.added = mkIf cnfg.enableXdgAssociations {
+      "text/html" = "microsoft-edge-beta.desktop";
+      "x-scheme-handler/http" = "microsoft-edge-beta.desktop";
+      "x-scheme-handler/https" = "microsoft-edge-beta.desktop";
+      "x-scheme-handler/about" = "microsoft-edge-beta.desktop";
+      "x-scheme-handler/unknown" = "microsoft-edge-beta.desktop";
+    };
   };
 }
