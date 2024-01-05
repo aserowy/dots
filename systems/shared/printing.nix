052291952d40fa7fefc15c40ead04fbe02153407
aserowy <serowy@hotmail.com>
2024-01-05 15:29:57 +0100

    printing: fixed printer
    Signed-off-by: aserowy <serowy@hotmail.com>

--- systems/shared/printing.nix
+++ systems/shared/printing.nix
@@ -1,22 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services = {
     printing = {
       enable = true;
-      browsing = true;
-      browsedConf = ''
-        BrowseDNSSDSubTypes _cups,_print
-        BrowseLocalProtocols all
-        BrowseRemoteProtocols all
-        CreateIPPPrinterQueues All
-
-        BrowseProtocols all
-      '';
+      drivers = [
+        pkgs.foo2zjs
+      ];
     };
 
     avahi = {
       enable = true;
       nssmdns4 = true;
+      openFirewall = true;
     };
   };
 }
