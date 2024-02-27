0fd47c48714cae0112902d11f4a47ab57f359cb9
aserowy <serowy@hotmail.com>
2024-02-27 11:00:47 +0100

    macos: added user .zshrc to set path and start nu
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/work-macos.nix
+++ home/work-macos.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
 
@@ -14,13 +14,20 @@
       docker.enable = true;
     };
 
-    file.".config/wezterm/wezterm.lua" = {
-      source = ./components/wezterm/wezterm.lua;
+    file = {
+      ".config/wezterm/wezterm.lua".source = ./components/wezterm/wezterm.lua;
+
+      ".zshrc".source = builtins.toFile "user-zshrc" ''
+        export PATH=/run/current-system/sw/bin:/etc/profiles/per-user/alexander.serowy/bin:$PATH
+
+        nu
+      '';
     };
 
-    packages = with pkgs; [
-      nerdfonts
-    ];
+    packages = with pkgs;
+      [
+        nerdfonts
+      ];
   };
 
   fonts.fontconfig.enable = true;
