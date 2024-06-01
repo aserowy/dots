24380bb1958b631ecbf31849a97057059f4e4e78
aserowy <serowy@hotmail.com>
2024-06-01 12:53:04 +0200

    gtk: added cursor theme
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/modules/gtk/default.nix
+++ home/modules/gtk/default.nix
@@ -7,37 +7,41 @@ in
 {
   options.home.modules.gtk.enable = mkEnableOption "gtk";
 
-  config =
-    let
-      theme = "Fluent-Dark-compact";
-      font = "Inter";
-      icon = "Fluent";
-    in
-    mkIf cnfg.enable {
-      home.packages = with pkgs; [
-        gnome.gnome-tweaks
-      ];
+  config = mkIf cnfg.enable {
+    home.packages = with pkgs; [
+      gnome.gnome-tweaks
+    ];
 
-      gtk = {
-        enable = true;
-        font = {
-          name = font;
-          size = 10;
-        };
-        iconTheme = {
-          name = icon;
-          package = pkgs.fluent-icon-theme;
-        };
-        theme = {
-          name = theme;
-          package = pkgs.fluent-gtk-theme;
-        };
-        gtk3.extraConfig = {
-          gtk-application-prefer-dark-theme = 1;
-        };
-        gtk4.extraConfig = {
-          gtk-application-prefer-dark-theme = 1;
-        };
+    gtk = {
+      enable = true;
+
+      cursorTheme = {
+        name = "Vanilla-DMZ";
+        package = pkgs.vanilla-dmz;
+      };
+
+      font = {
+        name = "Inter";
+        size = 10;
+      };
+
+      iconTheme = {
+        name = "Fluent";
+        package = pkgs.fluent-icon-theme;
+      };
+
+      theme = {
+        name = "Fluent-Dark-compact";
+        package = pkgs.fluent-gtk-theme;
+      };
+
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
       };
     };
+  };
 }
