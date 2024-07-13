de4dc7b780824146f55db3f874f3d83a0994a9d1
aserowy <serowy@hotmail.com>
2024-07-13 06:33:33 +0200

    gaming: added xwayland-satellite and niri integration to enable satellite x11 apps
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/modules/gaming/default.nix
+++ home/modules/gaming/default.nix
@@ -7,11 +7,21 @@ in
 {
   options.home.modules.gaming = {
     enable = mkEnableOption "gaming";
+
+    enableNiriIntegration = mkOption {
+      type = types.bool;
+      default = true;
+      description = ''
+        If enabled, xwayland-satellite gets started while running niri.
+      '';
+    };
   };
 
   config = mkIf cnfg.enable {
     home.packages = with pkgs; [
       gamescope
+      xwayland
+      xwayland-satellite
 
       (lutris.override {
         extraPkgs = pkgs: [
@@ -40,6 +50,14 @@ in
         icon = "lutris";
         categories = [ "Game" ];
       };
+    };
+
+    home.modules.niri.prependedConfig = mkIf cnfg.enableNiriIntegration ''
+      spawn-at-startup "xwayland-satellite"
+    '';
+
+    home.sessionVariables = mkIf cnfg.enableNiriIntegration {
+      DISPLAY = ":0";
     };
   };
 }
