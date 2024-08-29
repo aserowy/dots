bd6379f90a99c4d93f49b592e68cd4891384fdc0
aserowy <aserowy@users.noreply.github.com>
2024-08-29 19:19:21 +0000

    updated shake

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -139,11 +139,11 @@
   };
   vscode-extensions-remote-containers = {
     pname = "vscode-extensions-remote-containers";
-    version = "0.382.0";
+    version = "0.383.0";
     src = fetchurl {
-      url = "https://ms-vscode-remote.gallery.vsassets.io/_apis/public/gallery/publisher/ms-vscode-remote/extension/remote-containers/0.382.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
-      name = "remote-containers-0.382.0.zip";
-      sha256 = "sha256-KttnLz7wnNS/EgWeN0IJnHfRjAOAXZLYkHZqRRZ3JFo=";
+      url = "https://ms-vscode-remote.gallery.vsassets.io/_apis/public/gallery/publisher/ms-vscode-remote/extension/remote-containers/0.383.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage";
+      name = "remote-containers-0.383.0.zip";
+      sha256 = "sha256-yGxPcMm0icFiQQLJCWXIv8TovtQEBKxeatC/0rrtksA=";
     };
     publisher = "ms-vscode-remote";
     name = "remote-containers";
