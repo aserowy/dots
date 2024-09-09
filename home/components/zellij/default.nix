548de1d2e3328854acc7c978cd9aacef0531906a
aserowy <serowy@hotmail.com>
2024-09-09 06:45:17 +0200

    zellij: cleaned up default layout
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/components/zellij/default.nix
+++ home/components/zellij/default.nix
@@ -10,6 +10,7 @@ in
   config = mkIf cnfg.enable {
     home = {
       file.".config/zellij/config.kdl".source = ./config.kdl;
+      file.".config/zellij/layouts/compact.kdl".source = ./compact.kdl;
 
       packages = with pkgs; [
         zellij
