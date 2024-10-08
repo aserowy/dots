bc905a2804e75ff3c348fc92058b0c2a36b8974a
aserowy <serowy@hotmail.com>
2024-10-08 15:26:18 +0200

    foot: blulocoed
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/components/foot/default.nix
+++ home/components/foot/default.nix
@@ -33,6 +33,28 @@ in
           dpi-aware = mkIf cnfg.setDpiAware "yes";
           font = "JetBrainsMonoNerdFont:size=10";
         };
+        colors = {
+          background = "282c34";
+          foreground = "b9c0cb";
+          # Normal/regular colors (color palette 0-7)
+          regular0 = "41444d # black";
+          regular1 = "fc2f52  # red";
+          regular2 = "25a45c  # green";
+          regular3 = "ffcc00  # yellow";
+          regular4 = "3476ff  # blue";
+          regular5 = "7a82da  # magenta";
+          regular6 = "4483aa  # cyan";
+          regular7 = "cdd4e0  # white";
+          # Bright colors (color palette 8-15)
+          bright0 = "8f9aae   # bright black";
+          bright1 = "ff6480   # bright red";
+          bright2 = "3fc56b   # bright green";
+          bright3 = "f9c859   # bright yellow";
+          bright4 = "10b1fe   # bright blue";
+          bright5 = "ff78f8   # bright magenta";
+          bright6 = "5fb9bc   # bright cyan";
+          bright7 = "ffffff   # bright white";
+        };
       };
     };
 
