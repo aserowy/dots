5c3c592b7f9283844d73cf33f69d5d46185569f4
aserowy <serowy@hotmail.com>
2024-11-05 13:33:34 +0100

    console: pin tailspin till tests are fixed for 4.0.0
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/modules/console/default.nix
+++ home/modules/console/default.nix
@@ -3,6 +3,16 @@ with lib;
 
 let
   cnfg = config.home.modules.console;
+
+  system = "x86_64-linux";
+  pinned = import
+    (builtins.fetchGit {
+      name = "tailspin_3_0_1";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "05bbf675397d5366259409139039af8077d695ce";
+    })
+    { inherit system; };
 in
 {
   options.home.modules.console.enable = mkEnableOption "console";
@@ -27,7 +37,7 @@ in
         curl
         lazygit
         ncurses
-        tailspin
+        pinned.tailspin
 
         yeet
         fd
