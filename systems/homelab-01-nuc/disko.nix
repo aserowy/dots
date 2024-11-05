3d753518f1a5543e75b5351827efb40f845afec2
aserowy <serowy@hotmail.com>
2024-11-05 07:11:27 +0100

    homelab-01-nuc: remove unused mount in disko configuration
    Signed-off-by: aserowy <serowy@hotmail.com>

--- systems/homelab-01-nuc/disko.nix
+++ systems/homelab-01-nuc/disko.nix
@@ -36,8 +36,6 @@
                     mountOptions = [ "compress=zstd" ];
                     mountpoint = "/home";
                   };
-                  # Sub(sub)volume doesn't need a mountpoint as its parent is mounted
-                  "/home/serowy" = { };
                   # Parent is not mounted so the mountpoint must be set
                   "/nix" = {
                     mountOptions = [ "compress=zstd" "noatime" ];
