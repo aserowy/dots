53d31857a8b4b1d7367e498485b9fe30afd7ae29
aserowy <serowy@hotmail.com>
2024-11-08 17:04:09 +0100

    flake: move cdr generation into dev shell to fix flake outputs
    Signed-off-by: aserowy <serowy@hotmail.com>

--- .dev/default.nix
+++ .dev/default.nix
@@ -1,8 +1,24 @@
 {
-  self,
   nixidy,
   pkgs,
 }:
+let
+  generators = {
+    cilium = nixidy.packages.${pkgs.system}.generators.fromCRD {
+      name = "cilium";
+      src = pkgs.fetchFromGitHub {
+        owner = "cilium";
+        repo = "cilium";
+        rev = "v1.16.0";
+        hash = "sha256-LJrNGHF52hdKCuVwjvGifqsH+8hxkf/A3LZNpCHeR7E=";
+      };
+      crds = [
+        "pkg/k8s/apis/cilium.io/client/crds/v2/ciliumnetworkpolicies.yaml"
+        "pkg/k8s/apis/cilium.io/client/crds/v2/ciliumclusterwidenetworkpolicies.yaml"
+      ];
+    };
+  };
+in
 with pkgs;
 mkShell {
   packages = [
@@ -23,6 +39,6 @@ mkShell {
 
   shellHook = ''
     echo "generate cilium"
-    cat ${self.packages.${pkgs.system}.generators.cilium} > ./cluster/cdr/cilium.nix
+    cat ${generators.cilium} > ./cluster/cdr/cilium.nix
   '';
 }
