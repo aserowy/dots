dd5721549582ba03332ef3a96d7d2e2dc8cf5560
aserowy <serowy@hotmail.com>
2024-11-09 07:32:20 +0100

    homelab: add cilium policies to argocd
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/argocd.nix
+++ cluster/homelab/argocd.nix
@@ -22,5 +22,72 @@
         global.networkPolicy.create = true;
       };
     };
+
+        resources = {
+    ciliumNetworkPolicies = {
+      allow-world-egress.spec = {
+        endpointSelector.matchLabels."app.kubernetes.io/name" = "argocd-repo-server";
+        egress = [
+          {
+            toEndpoints = [
+              {
+                matchLabels = {
+                  "k8s:io.kubernetes.pod.namespace" = "kube-system";
+                  "k8s:k8s-app" = "kube-dns";
+                };
+              }
+            ];
+            toPorts = [
+              {
+                ports = [
+                  {
+                    port = "53";
+                    protocol = "ANY";
+                  }
+                ];
+                rules.dns = [
+                  { matchPattern = "*"; }
+                ];
+              }
+            ];
+          }
+          {
+            toFQDNs = [
+              { matchName = "github.com"; }
+            ];
+            toPorts = [
+              {
+                ports = [
+                  {
+                    port = "443";
+                    protocol = "TCP";
+                  }
+                ];
+              }
+            ];
+          }
+        ];
+      };
+
+      allow-kube-apiserver-egress.spec = {
+        endpointSelector.matchLabels."app.kubernetes.io/part-of" = "argocd";
+        egress = [
+          {
+            toEntities = [ "kube-apiserver" ];
+            toPorts = [
+              {
+                ports = [
+                  {
+                    port = "6443";
+                    protocol = "TCP";
+                  }
+                ];
+              }
+            ];
+          }
+        ];
+      };
+    };
+    };
   };
 }
