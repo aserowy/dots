798cd0061c16f70bd8f15982b08dc67d9ac6c643
aserowy <serowy@hotmail.com>
2024-11-09 06:59:43 +0100

    homelab: fix cilium namespace
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/argocd.nix
+++ cluster/homelab/argocd.nix
@@ -7,7 +7,18 @@
       chart = charts.argoproj.argo-cd;
 
       values = {
-        configs.params."server.insecure" = "true";
+        configs = {
+          # TODO: introduce module to configure from outside
+          cm."resource.exclusions" = ''
+            - apiGroups:
+              - cilium.io
+              kinds:
+                - CiliumIdentity
+              clusters:
+                - "*"
+          '';
+          params."server.insecure" = "true";
+        };
         global.networkPolicy.create = true;
       };
     };
--- cluster/homelab/cilium.nix
+++ cluster/homelab/cilium.nix
@@ -5,7 +5,9 @@
   ];
 
   applications.cilium = {
-    namespace = "cilium";
+    namespace = "kube-system";
+
+    compareOptions.serverSideDiff = true;
 
     helm.releases.cilium = {
       chart = charts.cilium.cilium;
@@ -35,7 +37,143 @@
     };
 
     resources = {
+      ciliumNetworkPolicies = {
+        # Allow hubble relay server egress to nodes
+        allow-hubble-relay-server-egress.spec = {
+          description = "Policy for egress from hubble relay to hubble server in Cilium agent.";
+          endpointSelector.matchLabels."app.kubernetes.io/name" = "hubble-relay";
+          egress = [
+            {
+              toEntities = [
+                "remote-node"
+                "host"
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "4244";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+
+        # Allow hubble UI to talk to hubble relay
+        allow-hubble-ui-relay-ingress.spec = {
+          description = "Policy for ingress from hubble UI to hubble relay.";
+          endpointSelector.matchLabels."app.kubernetes.io/name" = "hubble-relay";
+          ingress = [
+            {
+              fromEndpoints = [
+                {
+                  matchLabels."app.kubernetes.io/name" = "hubble-ui";
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "4245";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+
+        # Allow hubble UI to talk to kube-apiserver
+        allow-hubble-ui-kube-apiserver-egress.spec = {
+          description = "Allow Hubble UI to talk to kube-apiserver";
+          endpointSelector.matchLabels."app.kubernetes.io/name" = "hubble-ui";
+          egress = [
+            {
+              toEntities = [ "kube-apiserver" ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "6443";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+
+        # Allow kube-dns to talk to upstream DNS
+        allow-kube-dns-upstream-egress.spec = {
+          description = "Policy for egress to allow kube-dns to talk to upstream DNS.";
+          endpointSelector.matchLabels.k8s-app = "kube-dns";
+          egress = [
+            {
+              toEntities = [ "world" ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "53";
+                      protocol = "UDP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+
+        # Allow CoreDNS to talk to kube-apiserver
+        allow-kube-dns-apiserver-egress.spec = {
+          description = "Allow coredns to talk to kube-apiserver.";
+          endpointSelector.matchLabels.k8s-app = "kube-dns";
+          egress = [
+            {
+              toEntities = [ "kube-apiserver" ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "6443";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+
+        # Allow hubble-generate-certs job to talk to kube-apiserver
+        allow-hubble-generate-certs-apiserver-egress.spec = {
+          description = "Allow hubble-generate-certs job to talk to kube-apiserver.";
+          endpointSelector.matchLabels."batch.kubernetes.io/job-name" = "hubble-generate-certs";
+          egress = [
+            {
+              toEntities = [ "kube-apiserver" ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "6443";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
+      };
+
       ciliumClusterwideNetworkPolicies = {
+        # Allow all cilium endpoints to talk egress to each other
         allow-internal-egress.spec = {
           description = "Policy to allow all Cilium managed endpoint to talk to all other cilium managed endpoints on egress";
           endpointSelector = { };
@@ -46,6 +184,7 @@
           ];
         };
 
+        # Allow all health checks
         cilium-health-checks.spec = {
           endpointSelector.matchLabels."reserved:health" = "";
           ingress = [
@@ -60,6 +199,7 @@
           ];
         };
 
+        # Allow all cilium managed endpoints to talk to cluster dns
         allow-kube-dns-cluster-ingress.spec = {
           description = "Policy for ingress allow to kube-dns from all Cilium managed endpoints in the cluster.";
           endpointSelector.matchLabels = {
