178f4234971c811baabb96337fa11364a0ce3fdf
aserowy <serowy@hotmail.com>
2025-05-22 06:19:50 +0200

    homeassistant: strip home for serowy
    Signed-off-by: aserowy <serowy@hotmail.com>

--- home/homeassistant.nix
+++ home/homeassistant.nix
@@ -3,15 +3,4 @@
   imports = [
     ./base.nix
   ];
-
-  home = {
-    components = {
-      docker.enable = true;
-    };
-
-    modules = {
-      console.enable = true;
-      neocode.parallelTsBuild = false;
-    };
-  };
 }
