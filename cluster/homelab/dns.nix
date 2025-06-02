de42fc19a51b67a28b133bf61a12e12c52159c5f
aserowy <serowy@hotmail.com>
2025-06-02 18:27:44 +0200

    cluster: add pvc to pihole
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/dns.nix
+++ cluster/homelab/dns.nix
@@ -6,6 +6,28 @@
 
     helm.releases.pihole = {
       chart = charts.mojo2600.pihole;
+
+      values = {
+        persistentVolumeClaim = {
+          enabled = true;
+        };
+      };
     };
+
+    yamls = [
+      ''
+        apiVersion: v1
+        kind: PersistentVolumeClaim
+        metadata:
+          name: pihole-pvc
+        spec:
+          storageClassName: longhorn
+          accessModes:
+            - ReadWriteMany
+          resources:
+            requests:
+              storage: 1Gi
+      ''
+    ];
   };
 }
