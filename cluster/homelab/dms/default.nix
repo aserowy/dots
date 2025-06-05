b968176d26daa2a3735ee93e047ace5e528913ee
aserowy <serowy@hotmail.com>
2025-06-05 07:59:25 +0200

    cluster: add postgresql for paperless
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/dms/default.nix
+++ cluster/homelab/dms/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ charts, lib, ... }:
 let
   namespace = "dms";
 in
@@ -7,16 +7,30 @@ in
     inherit namespace;
     createNamespace = true;
 
-    helm.releases.valkey = {
-      # TODO: renovate update template for this and remove nix helm?
-      chart = lib.helm.downloadHelmChart {
-        repo = "https://charts.bitnami.com/bitnami/";
-        chart = "valkey";
-        version = "3.0.9";
-        chartHash = "sha256-zSLEopYHW05p7OxZlcusR9SQcmtGnKji6CcQPl9s0xA=";
+    helm.releases = {
+      postgresql = {
+        chart = charts.bitnami.postgresql;
+
+        values = {
+          auth = {
+            database = "paperless";
+            username = "paperless";
+            existingSecret = "postgresql";
+          };
+        };
       };
 
-      values = {
+      valkey = {
+        # TODO: renovate update template for this and remove nix helm?
+        chart = lib.helm.downloadHelmChart {
+          repo = "https://charts.bitnami.com/bitnami/";
+          chart = "valkey";
+          version = "3.0.9";
+          chartHash = "sha256-zSLEopYHW05p7OxZlcusR9SQcmtGnKji6CcQPl9s0xA=";
+        };
+
+        values = {
+        };
       };
     };
 
