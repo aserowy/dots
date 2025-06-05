40ae3dc882ef832de3744a0dddf2bdfeb3fac940
aserowy <serowy@hotmail.com>
2025-06-05 18:00:19 +0200

    cluster: add server side apply and prune to monitoring
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/monitoring.nix
+++ cluster/homelab/monitoring.nix
@@ -4,6 +4,11 @@
     namespace = "monitoring";
     createNamespace = true;
 
+    syncPolicy.syncOptions = {
+      replace = true;
+      serverSideApply = true;
+    };
+
     helm.releases.kube-prome = {
       chart = charts.prometheus-community.kube-prometheus-stack;
     };
