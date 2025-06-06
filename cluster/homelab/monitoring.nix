25ee39fb97392735ec12791defb00a16a33669c8
aserowy <serowy@hotmail.com>
2025-06-06 07:31:03 +0200

    cluster: remove monitoring for cleanup
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/monitoring.nix
+++ cluster/homelab/monitoring.nix
@@ -9,8 +9,13 @@
       serverSideApply = true;
     };
 
-    helm.releases.kube-prometheus = {
+    helm.releases.kube-prometheus-stack = {
       chart = charts.prometheus-community.kube-prometheus-stack;
+
+      values = {
+        nameOverride = "kube-prometheus";
+        fullnameOverride = "kube-prometheus";
+      };
     };
   };
 }
