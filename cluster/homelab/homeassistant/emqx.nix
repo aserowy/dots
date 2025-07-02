1ee8205758b1d7d883ae7d7b3f8ecc4e81f74e76
aserowy <serowy@hotmail.com>
2025-07-02 07:32:09 +0200

    cluster: add emqx as mqtt broker in homeassistant
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/homeassistant/emqx.nix
+++ cluster/homelab/homeassistant/emqx.nix
@@ -1,15 +1,12 @@
-{ application, charts, ... }:
+{
+  application,
+  charts,
+  ...
+}:
 {
   applications."${application}" = {
-    helm.releases = {
-      # FIX: replace with nixhelm entry after pr merged
-      # chart = charts.emqx.emqx;
-      chart = {
-        repo = "https://repos.emqx.io/charts/";
-        chart = "emqx";
-        version = "5.8.6";
-        chartHash = "sha256-no99jOD0yiBoShfBFQK9MZL8Yu9WHVz0iMPXAPi9q3w=";
-      };
+    helm.releases.emqx = {
+      chart = charts.emqx.emqx;
 
       values = { };
     };
