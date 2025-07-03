7aa74972f1b74d45a1320f80d482281da84690b5
aserowy <serowy@hotmail.com>
2025-07-03 18:38:42 +0200

    cluster: reenable emqx dashboard
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/homeassistant/emqx-operator.nix
+++ cluster/homelab/homeassistant/emqx-operator.nix
@@ -16,27 +16,39 @@
       };
     };
 
+    yamls = [
+      # TODO: update with renovate
+      ''
+        apiVersion: apps.emqx.io/v2beta1
+        kind: EMQX
+        metadata:
+           name: emqx
+        spec:
+           image: emqx/emqx:5.9.1
+      ''
+    ];
+
     resources = {
       ingressRoutes = {
-        # emqx-dashboard-route.spec = {
-        #   entryPoints = [
-        #     "websecure"
-        #   ];
-        #   routes = [
-        #     {
-        #       match = "Host(`mqtt.anderwerse.de`)";
-        #       kind = "Rule";
-        #       services = [
-        #         {
-        #           inherit namespace;
-        #           name = "emqx";
-        #           port = 18083;
-        #         }
-        #       ];
-        #     }
-        #   ];
-        #   tls.secretName = "anderwersede-tls-certificate";
-        # };
+        emqx-dashboard-route.spec = {
+          entryPoints = [
+            "websecure"
+          ];
+          routes = [
+            {
+              match = "Host(`mqtt.anderwerse.de`)";
+              kind = "Rule";
+              services = [
+                {
+                  inherit namespace;
+                  name = "emqx";
+                  port = 18083;
+                }
+              ];
+            }
+          ];
+          tls.secretName = "anderwersede-tls-certificate";
+        };
       };
     };
   };
