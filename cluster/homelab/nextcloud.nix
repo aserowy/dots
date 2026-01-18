cdbebba6654228bbc816ea4df49b63323294ce04
aserowy <serowy@hotmail.com>
2026-01-18 08:48:16 +0100

    homelab: change to all default nextcloud values
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/nextcloud.nix
+++ cluster/homelab/nextcloud.nix
@@ -13,41 +13,41 @@ in
 
       values = {
         # conjob.enabled = true;
-        internalDatabase.enabled = false;
-        nextcloud = {
-          # existingSecret = {
-          #   enabled = true;
-          #   secretName = "nextcloud";
-          #   usernameKey = "username";
-          #   passwordKey = "password";
-          # };
-          # host = "nextcloud.anderwerse.de";
-          # trustedDomains = [ "nextcloud.anderwerse.de" ];
-        };
-        persistence = {
-          enabled = true;
-          storageClass = "longhorn";
-          nextcloudData = {
-            enabled = true;
-            storageClass = "longhorn";
-            size = "20Gi";
-          };
-        };
-        postgresql = {
-          enabled = true;
-          # global.postgresql.auth = {
-          #   existingSecret = "database";
-          #   secretKeys = {
-          #     adminPasswordKey = "admin_password";
-          #     userPasswordKey = "user_password";
-          #     replicationPasswordKey = "replication_password";
-          #   };
-          # };
-          primary.persistence = {
-            enabled = true;
-            storageClass = "longhorn";
-          };
-        };
+        # internalDatabase.enabled = false;
+        # nextcloud = {
+        # existingSecret = {
+        #   enabled = true;
+        #   secretName = "nextcloud";
+        #   usernameKey = "username";
+        #   passwordKey = "password";
+        # };
+        # host = "nextcloud.anderwerse.de";
+        # trustedDomains = [ "nextcloud.anderwerse.de" ];
+        # };
+        # persistence = {
+        #   enabled = true;
+        #   storageClass = "longhorn";
+        #   nextcloudData = {
+        #     enabled = true;
+        #     storageClass = "longhorn";
+        #     size = "20Gi";
+        #   };
+        # };
+        # postgresql = {
+        #   enabled = true;
+        # global.postgresql.auth = {
+        #   existingSecret = "database";
+        #   secretKeys = {
+        #     adminPasswordKey = "admin_password";
+        #     userPasswordKey = "user_password";
+        #     replicationPasswordKey = "replication_password";
+        #   };
+        # };
+        #   primary.persistence = {
+        #     enabled = true;
+        #     storageClass = "longhorn";
+        #   };
+        # };
         livenessProbe.initialDelaySeconds = 30;
         readinessProbe.initialDelaySeconds = 30;
       };
