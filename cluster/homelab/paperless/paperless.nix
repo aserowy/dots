22b171d6de24ced87d10b939b4f46d37b66908bf
aserowy <serowy@hotmail.com>
2026-01-19 17:31:19 +0100

    homelab: add paperless file importer
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/paperless/paperless.nix
+++ cluster/homelab/paperless/paperless.nix
@@ -95,6 +95,17 @@ in
                     type = "RuntimeDefault";
                   };
                 };
+                initContainers = [
+                  {
+                    name = "await-db-init";
+                    image = "busybox:1.37"; # docker/busybox@semver-coerced
+                    command = [
+                      "sh"
+                      "-c"
+                      "until nc -z db 5432; do echo 'Waiting for Postgres...'; sleep 5; done"
+                    ];
+                  }
+                ];
                 containers = [
                   {
                     name = "paperless";
@@ -201,6 +212,14 @@ in
                         memory = "2Gi";
                       };
                     };
+                    startupProbe = {
+                      tcpSocket.port = 8000;
+                      initialDelaySeconds = 45;
+                      periodSeconds = 10;
+                      failureThreshold = 5;
+                    };
+                    livenessProbe.tcpSocket.port = 8000;
+                    readinessProbe.tcpSocket.port = 8000;
                     volumeMounts = [
                       {
                         name = "run";
@@ -255,6 +274,102 @@ in
                     persistentVolumeClaim.claimName = paperless-media-pvc;
                   }
                 ];
+              };
+            };
+          };
+        };
+      };
+
+      cronJobs = {
+        "paperless-nextcloud-importer" = {
+          metadata = {
+            inherit namespace;
+            name = "paperless-nextcloud-importer";
+          };
+          spec = {
+            schedule = "*/5 * * * *";
+            jobTemplate = {
+              spec = {
+                template = {
+                  spec = {
+                    restartPolicy = "Never";
+                    affinity = {
+                      podAffinity = {
+                        requiredDuringSchedulingIgnoredDuringExecution = [
+                          {
+                            labelSelector = {
+                              matchLabels = {
+                                name = "paperless";
+                              };
+                            };
+                            topologyKey = "kubernetes.io/hostname";
+                          }
+                        ];
+                      };
+                    };
+                    containers = [
+                      {
+                        name = "rclone-move";
+                        image = "rclone/rclone:1.69.1";
+                        imagePullPolicy = "IfNotPresent";
+                        args = [
+                          "move"
+                          "nextcloud:/Paperless/"
+                          "/consume/"
+                          "--transfers=1"
+                          "--checkers=1"
+                          "--no-traverse"
+                          "--retries=3"
+                          "--log-level=INFO"
+                        ];
+                        env = [
+                          {
+                            name = "RCLONE_CONFIG_NEXTCLOUD_TYPE";
+                            value = "webdav";
+                          }
+                          {
+                            name = "RCLONE_CONFIG_NEXTCLOUD_URL";
+                            value = "https://nextcloud.anderwerse.de";
+                          }
+                          {
+                            name = "RCLONE_CONFIG_NEXTCLOUD_VENDOR";
+                            value = "nextcloud";
+                          }
+                          {
+                            name = "RCLONE_CONFIG_NEXTCLOUD_USER";
+                            valueFrom = {
+                              secretKeyRef = {
+                                name = "nextcloud";
+                                key = "user";
+                              };
+                            };
+                          }
+                          {
+                            name = "RCLONE_CONFIG_NEXTCLOUD_PASS";
+                            valueFrom = {
+                              secretKeyRef = {
+                                name = "nextcloud";
+                                key = "password";
+                              };
+                            };
+                          }
+                        ];
+                        volumeMounts = [
+                          {
+                            name = "consume";
+                            mountPath = "/consume";
+                          }
+                        ];
+                      }
+                    ];
+                    volumes = [
+                      {
+                        name = "consume";
+                        persistentVolumeClaim.claimName = paperless-consume-pvc;
+                      }
+                    ];
+                  };
+                };
               };
             };
           };
