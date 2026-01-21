ce2a12a076cbfc3706c5ceece276d71d200c5c03
aserowy <serowy@hotmail.com>
2026-01-21 18:44:55 +0100

    homelab: move issuer and secrets to certificates namespace
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/certificates.nix
+++ cluster/homelab/certificates.nix
@@ -22,246 +22,277 @@ in
       };
     };
 
+    yamls = [
+      (builtins.readFile ./certificates-secrets.sops.yaml)
+    ];
+
     # NOTE: https://cert-manager.io/docs/installation/best-practice/#network-requirements
-    resources.ciliumNetworkPolicies = {
-      cainjector = {
-        apiVersion = "cilium.io/v2";
-        kind = "CiliumNetworkPolicy";
-        metadata = {
-          inherit namespace;
-        };
-        spec = {
-          endpointSelector.matchLabels = {
-            "app.kubernetes.io/name" = "cainjector";
-          };
-          ingress = [
-            # NOTE: metrics collector must get unblocked here
-            { }
-          ];
-          egress = [
+    resources = {
+      clusterIssuers = {
+        azure-acme-issuer.spec.acme = {
+          email = "serowy@hotmail.com";
+          server = "https://acme-v02.api.letsencrypt.org/directory";
+          privateKeySecretRef.name = "azure-issuer-account-key";
+          solvers = [
             {
-              toEntities = [
-                "host"
-                "kube-apiserver"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "6443";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              toEndpoints = [
-                {
-                  matchLabels = {
-                    "io.kubernetes.pod.namespace" = "kube-system";
-                    "k8s-app" = "kube-dns";
+              dns01 = {
+                azureDNS = {
+                  clientID = "e48f89f8-a7ac-45c6-8d9c-10edbf3365cb";
+                  clientSecretSecretRef = {
+                    name = "azure-acme-environment";
+                    key = "AZURE_CLIENT_SECRET";
                   };
-                }
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "53";
-                      protocol = "UDP";
-                    }
-                  ];
-                }
-              ];
+                  subscriptionID = "b0f74bb3-5530-4ad4-a307-dc6a96b371c0";
+                  tenantID = "fe7eb9da-a96c-4ffd-8fe0-fd1ec4f77439";
+                  resourceGroupName = "rg_homelab";
+                  hostedZoneName = "anderwerse.de";
+                };
+              };
             }
           ];
         };
       };
 
-      cert-manager = {
-        apiVersion = "cilium.io/v2";
-        kind = "CiliumNetworkPolicy";
-        metadata = {
-          inherit namespace;
-        };
-        spec = {
-          endpointSelector.matchLabels = {
-            "app.kubernetes.io/name" = "cert-manager";
+      ciliumNetworkPolicies = {
+        cainjector = {
+          apiVersion = "cilium.io/v2";
+          kind = "CiliumNetworkPolicy";
+          metadata = {
+            inherit namespace;
           };
-          ingress = [
-            # NOTE: metrics collector must get unblocked here
-            {
-              fromEntities = [
-                "host"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "9403";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-          ];
-          egress = [
-            {
-              toEntities = [
-                "kube-apiserver"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "6443";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              toEntities = [
-                "world"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "443";
-                      protocol = "TCP";
-                    }
-                    {
-                      port = "53";
-                      protocol = "UDP";
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              toEndpoints = [
-                {
-                  matchLabels = {
-                    "io.kubernetes.pod.namespace" = "kube-system";
-                    "k8s-app" = "kube-dns";
-                  };
-                }
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "53";
-                      protocol = "UDP";
-                    }
-                  ];
-                }
-              ];
-            }
-          ];
+          spec = {
+            endpointSelector.matchLabels = {
+              "app.kubernetes.io/name" = "cainjector";
+            };
+            ingress = [
+              # NOTE: metrics collector must get unblocked here
+              { }
+            ];
+            egress = [
+              {
+                toEntities = [
+                  "host"
+                  "kube-apiserver"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "6443";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                toEndpoints = [
+                  {
+                    matchLabels = {
+                      "io.kubernetes.pod.namespace" = "kube-system";
+                      "k8s-app" = "kube-dns";
+                    };
+                  }
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "53";
+                        protocol = "UDP";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+          };
         };
-      };
 
-      webhook = {
-        apiVersion = "cilium.io/v2";
-        kind = "CiliumNetworkPolicy";
-        metadata = {
-          inherit namespace;
-        };
-        spec = {
-          endpointSelector.matchLabels = {
-            "app.kubernetes.io/name" = "webhook";
+        cert-manager = {
+          apiVersion = "cilium.io/v2";
+          kind = "CiliumNetworkPolicy";
+          metadata = {
+            inherit namespace;
           };
-          ingress = [
-            # NOTE: metrics collector must get unblocked here
-            {
-              fromEntities = [
-                "host"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "6080";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              fromEntities = [
-                "kube-apiserver"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "443";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              # NOTE: startupapicheck node
-              fromEntities = [
-                "remote-node"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "10250";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-          ];
-          egress = [
-            {
-              toEntities = [
-                "kube-apiserver"
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "6443";
-                      protocol = "TCP";
-                    }
-                  ];
-                }
-              ];
-            }
-            {
-              toEndpoints = [
-                {
-                  matchLabels = {
-                    "io.kubernetes.pod.namespace" = "kube-system";
-                    "k8s-app" = "kube-dns";
-                  };
-                }
-              ];
-              toPorts = [
-                {
-                  ports = [
-                    {
-                      port = "53";
-                      protocol = "UDP";
-                    }
-                  ];
-                }
-              ];
-            }
-          ];
+          spec = {
+            endpointSelector.matchLabels = {
+              "app.kubernetes.io/name" = "cert-manager";
+            };
+            ingress = [
+              # NOTE: metrics collector must get unblocked here
+              {
+                fromEntities = [
+                  "host"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "9403";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+            egress = [
+              {
+                toEntities = [
+                  "kube-apiserver"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "6443";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                toEntities = [
+                  "world"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "443";
+                        protocol = "TCP";
+                      }
+                      {
+                        port = "53";
+                        protocol = "UDP";
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                toEndpoints = [
+                  {
+                    matchLabels = {
+                      "io.kubernetes.pod.namespace" = "kube-system";
+                      "k8s-app" = "kube-dns";
+                    };
+                  }
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "53";
+                        protocol = "UDP";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+          };
+        };
+
+        webhook = {
+          apiVersion = "cilium.io/v2";
+          kind = "CiliumNetworkPolicy";
+          metadata = {
+            inherit namespace;
+          };
+          spec = {
+            endpointSelector.matchLabels = {
+              "app.kubernetes.io/name" = "webhook";
+            };
+            ingress = [
+              # NOTE: metrics collector must get unblocked here
+              {
+                fromEntities = [
+                  "host"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "6080";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                fromEntities = [
+                  "kube-apiserver"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "443";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                # NOTE: startupapicheck node
+                fromEntities = [
+                  "remote-node"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "10250";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+            egress = [
+              {
+                toEntities = [
+                  "kube-apiserver"
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "6443";
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                ];
+              }
+              {
+                toEndpoints = [
+                  {
+                    matchLabels = {
+                      "io.kubernetes.pod.namespace" = "kube-system";
+                      "k8s-app" = "kube-dns";
+                    };
+                  }
+                ];
+                toPorts = [
+                  {
+                    ports = [
+                      {
+                        port = "53";
+                        protocol = "UDP";
+                      }
+                    ];
+                  }
+                ];
+              }
+            ];
+          };
         };
       };
     };
--- cluster/homelab/loadbalancer.nix
+++ cluster/homelab/loadbalancer.nix
@@ -65,36 +65,7 @@ in
       };
     };
 
-    yamls = [
-      (builtins.readFile ./loadbalancer-secrets.sops.yaml)
-    ];
-
     resources = {
-      clusterIssuers = {
-        azure-acme-issuer.spec.acme = {
-          email = "serowy@hotmail.com";
-          server = "https://acme-v02.api.letsencrypt.org/directory";
-          privateKeySecretRef.name = "azure-issuer-account-key";
-          solvers = [
-            {
-              dns01 = {
-                azureDNS = {
-                  clientID = "e48f89f8-a7ac-45c6-8d9c-10edbf3365cb";
-                  clientSecretSecretRef = {
-                    name = "azure-acme-environment";
-                    key = "AZURE_CLIENT_SECRET";
-                  };
-                  subscriptionID = "b0f74bb3-5530-4ad4-a307-dc6a96b371c0";
-                  tenantID = "fe7eb9da-a96c-4ffd-8fe0-fd1ec4f77439";
-                  resourceGroupName = "rg_homelab";
-                  hostedZoneName = "anderwerse.de";
-                };
-              };
-            }
-          ];
-        };
-      };
-
       certificates = {
         anderwersede-tls-certificate.spec = {
           secretName = "anderwersede-tls-certificate";
