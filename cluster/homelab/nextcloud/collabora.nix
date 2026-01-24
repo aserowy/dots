0f10f74c6b1ca23329abd33f876836ef1df3bdf7
aserowy <serowy@hotmail.com>
2026-01-24 17:56:35 +0100

    homelab: add cilium network policy for collabora
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/nextcloud/collabora.nix
+++ cluster/homelab/nextcloud/collabora.nix
@@ -41,42 +41,139 @@
     };
 
     resources = {
-      ingresses = {
-        collabora = {
-          metadata = {
-            inherit namespace;
-            annotations = {
-              "cert-manager.io/cluster-issuer" = "azure-acme-issuer";
-              "haproxy.org/timeout-tunnel" = "3600s";
-              "haproxy.org/backend-config-snippet" =
-                "balance url_param WOPISrc check_post
+      ingresses.collabora = {
+        metadata = {
+          inherit namespace;
+          annotations = {
+            "cert-manager.io/cluster-issuer" = "azure-acme-issuer";
+            "haproxy.org/timeout-tunnel" = "3600s";
+            "haproxy.org/backend-config-snippet" =
+              "balance url_param WOPISrc check_post
                 hash-type consistent";
-            };
           };
-          spec = {
-            ingressClassName = "haproxy";
-            tls = [
-              {
-                hosts = [ "collabora.anderwerse.de" ];
-                secretName = "collabora-tls";
-              }
-            ];
-            rules = [
-              {
-                host = "collabora.anderwerse.de";
-                http.paths = [
-                  {
-                    pathType = "ImplementationSpecific";
-                    path = "/";
-                    backend.service = {
-                      name = "collabora-collabora-online";
-                      port.number = 9980;
-                    };
-                  }
-                ];
-              }
-            ];
-          };
+        };
+        spec = {
+          ingressClassName = "haproxy";
+          tls = [
+            {
+              hosts = [ "collabora.anderwerse.de" ];
+              secretName = "collabora-tls";
+            }
+          ];
+          rules = [
+            {
+              host = "collabora.anderwerse.de";
+              http.paths = [
+                {
+                  pathType = "ImplementationSpecific";
+                  path = "/";
+                  backend.service = {
+                    name = "collabora-collabora-online";
+                    port.number = 9980;
+                  };
+                }
+              ];
+            }
+          ];
+        };
+      };
+
+      ciliumNetworkPolicies.collabora = {
+        apiVersion = "cilium.io/v2";
+        kind = "CiliumNetworkPolicy";
+        metadata = {
+          inherit namespace;
+        };
+        spec = {
+          endpointSelector.matchLabels."app.kubernetes.io/name" = "collabora-collabora-online";
+          ingress = [
+            {
+              fromEntities = [
+                "host"
+              ];
+              fromEndpoints = [
+                {
+                  matchLabels = {
+                    "io.kubernetes.pod.namespace" = "haproxy";
+                    "app.kubernetes.io/name" = "kubernetes-ingress";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "9980";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+          egress = [
+            {
+              toEndpoints = [
+                {
+                  matchLabels = {
+                    "io.kubernetes.pod.namespace" = "kube-system";
+                    "k8s-app" = "kube-dns";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "53";
+                      protocol = "UDP";
+                    }
+                  ];
+                }
+              ];
+            }
+            {
+              # TODO: remove after traefik migration
+              toEndpoints = [
+                {
+                  matchLabels = {
+                    "io.kubernetes.pod.namespace" = "loadbalancer";
+                    "app.kubernetes.io/role" = "entrypoint";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "443";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+            {
+              toEndpoints = [
+                {
+                  matchLabels = {
+                    "io.kubernetes.pod.namespace" = "haproxy";
+                    "app.kubernetes.io/name" = "kubernetes-ingress";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "8443";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
         };
       };
     };
