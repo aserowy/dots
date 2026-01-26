15785cc69f50bd496233fa516c89e424cc4a9cad
aserowy <serowy@hotmail.com>
2026-01-26 07:20:58 +0100

    homelab: add netpol for grafana
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/homelab/monitoring.nix
+++ cluster/homelab/monitoring.nix
@@ -73,36 +73,158 @@ in
       (builtins.readFile ./monitoring-secrets.sops.yaml)
     ];
 
-    resources.ingresses.grafana = {
-      metadata = {
-        inherit namespace;
-        annotations = {
-          "cert-manager.io/cluster-issuer" = "azure-acme-issuer";
+    resources = {
+      ingresses.grafana = {
+        metadata = {
+          inherit namespace;
+          annotations = {
+            "cert-manager.io/cluster-issuer" = "azure-acme-issuer";
+          };
+        };
+        spec = {
+          ingressClassName = "haproxy";
+          tls = [
+            {
+              hosts = [ "grafana.cluster.anderwerse.de" ];
+              secretName = "grafana-tls";
+            }
+          ];
+          rules = [
+            {
+              host = "grafana.cluster.anderwerse.de";
+              http.paths = [
+                {
+                  pathType = "Prefix";
+                  path = "/";
+                  backend.service = {
+                    name = "kube-prometheus-stack-grafana";
+                    port.number = 80;
+                  };
+                }
+              ];
+            }
+          ];
         };
       };
-      spec = {
-        ingressClassName = "haproxy";
-        tls = [
-          {
-            hosts = [ "grafana.cluster.anderwerse.de" ];
-            secretName = "grafana-tls";
-          }
-        ];
-        rules = [
-          {
-            host = "grafana.cluster.anderwerse.de";
-            http.paths = [
-              {
-                pathType = "Prefix";
-                path = "/";
-                backend.service = {
-                  name = "kube-prometheus-stack-grafana";
-                  port.number = 80;
-                };
-              }
-            ];
-          }
-        ];
+
+      ciliumNetworkPolicies.grafana = {
+        apiVersion = "cilium.io/v2";
+        kind = "CiliumNetworkPolicy";
+        metadata = {
+          inherit namespace;
+        };
+        spec = {
+          endpointSelector = {
+            matchLabels = {
+              "app.kubernetes.io/name" = "grafana";
+            };
+          };
+          ingress = [
+            {
+              fromEndpoints = [
+                {
+                  matchLabels = {
+                    "io.kubernetes.pod.namespace" = "haproxy";
+                    "app.kubernetes.io/name" = "kubernetes-ingress";
+                  };
+                }
+                {
+                  matchLabels = {
+                    "app.kubernetes.io/name" = "prometheus";
+                  };
+                }
+              ];
+              fromEntities = [
+                "host"
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "3000";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+          egress = [
+            {
+              toEntities = [
+                "kube-apiserver"
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "6443";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+            {
+              toEndpoints = [
+                {
+                  matchLabels = {
+                    "io.kubernetes.pod.namespace" = "kube-system";
+                    "k8s-app" = "kube-dns";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "53";
+                      protocol = "UDP";
+                    }
+                  ];
+                }
+              ];
+            }
+            {
+              toEndpoints = [
+                {
+                  matchLabels = {
+                    "app.kubernetes.io/name" = "prometheus";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "9090";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+            {
+              toEndpoints = [
+                {
+                  matchLabels = {
+                    "app" = "longhorn-manager";
+                  };
+                }
+              ];
+              toPorts = [
+                {
+                  ports = [
+                    {
+                      port = "9500";
+                      protocol = "TCP";
+                    }
+                  ];
+                }
+              ];
+            }
+          ];
+        };
       };
     };
   };
