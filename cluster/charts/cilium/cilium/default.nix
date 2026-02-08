be9579257eaa1fa298e5cdcd553d6a473571453b
aserowy <serowy@hotmail.com>
2026-02-08 16:25:23 +0100

    renovate: add digest to chart as sha check for downloaded archive
    Signed-off-by: aserowy <serowy@hotmail.com>

--- cluster/charts/cilium/cilium/default.nix
+++ cluster/charts/cilium/cilium/default.nix
@@ -2,5 +2,6 @@
   repo = "https://helm.cilium.io/";
   chart = "cilium";
   version = "1.18.6";
+  digest = "fb2af7f257395f5f9e5c2144febabf2b49b883f222345247ba107e36edb8794b";
   chartHash = "sha256-DxOpCAvYFrSUxKgWOV3wU7PO1qXhEgkdX7VdVxHTp7M=";
 }
